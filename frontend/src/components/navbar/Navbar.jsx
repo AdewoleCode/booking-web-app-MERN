1d0d6545e7d83af761970e1ece1821f737f7992c
adewole <adeadewole1712@gmail.com>
2023-01-23 23:25:56 +0100

    logout funtionality added

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,15 +1,23 @@
 import "./Navbar.css"
-import { Link } from 'react-router-dom'
-import { useSelector } from "react-redux";
+import { Link, useNavigate } from 'react-router-dom'
+import { useSelector, useDispatch } from "react-redux";
+import { AuthActions } from "../../redux/slices/AuthSlice";
 
 
 const Navbar = () => {
-
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
   const user = useSelector(state => state.auth.user)
+
+  const handleLogout = () => {
+    dispatch(AuthActions.logout())
+    localStorage.clear('user')
+    navigate('/')
+  }
 
 
   return (
-
+    
     <div className="navbar">
       <div className="navContainer">
         <span className="logo">
@@ -23,12 +31,19 @@ const Navbar = () => {
           </Link>
         </span>
         {
-          user ? user.username : (
-            <div className="navItems">
-              <button className="reg navButton" > <Link style={{ textDecoration: "none", color: "inherit" }} to='/register'>Register</Link></button>
-              <button className="log navButton" ><Link style={{ textDecoration: "none", color: "inherit" }} to='/login'>Login</Link></button>
+          user ? (
+            <div>
+              <h1>user.username</h1>
+              <button onClick={handleLogout} className="log navButton">Logout</button>
             </div>
           )
+
+            : (
+              <div className="navItems">
+                <button className="reg navButton" > <Link style={{ textDecoration: "none", color: "inherit" }} to='/register'>Register</Link></button>
+                <button className="log navButton" ><Link style={{ textDecoration: "none", color: "inherit" }} to='/login'>Login</Link></button>
+              </div>
+            )
         }
       </div>
     </div>
